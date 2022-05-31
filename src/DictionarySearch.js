144fcaa8c809c6aa4076f88054560a8ff964695a
Seo-Pt <seonah.pari.pt@gmail.com>
2022-05-31 15:19:17 +0200

    Added API documentation URL

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -18,6 +18,7 @@ export default function DictionarySearch(){
     
     function search(event){
         event.preventDefault();
+        // API doc source: https://dictionaryapi.dev/
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse)
     }
