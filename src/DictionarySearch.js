3172416c60d2d5d26c1180baa142329d201231d7
Seo-Pt <seonah.pari.pt@gmail.com>
2022-05-31 14:26:03 +0200

    Created basic form with search bar

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -4,6 +4,10 @@ import "./DictionarySearch.css";
 export default function DictionarySearch(){
     return (
         <div className="DictionarySearch">
-            <p>Hello from DictionarySearch</p>
+            <form>
+                <input type="search" className="search-engine" placeholder="Search for a word..." />
+                <input type="submit" className="search-submit-btn" value="🔎Go!" />
+            </form>
+            
         </div>);
 }
