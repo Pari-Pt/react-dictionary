4f1e151c65e6e296f1d7aed1f8479364b03dd5df
Seo-Pt <seonah.pari.pt@gmail.com>
2022-05-31 14:37:02 +0200

    Imported and used {useState} to track value of search engine, and alert value upon submission

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch(){
     
+    let [keyword, setKeyword] = useState("");
+
+    function handleChanges(event){
+        setKeyword(event.target.value)
+    }
+    
+    
     function search(event){
         event.preventDefault();
-        alert("test");
+        alert(keyword);
     }
+
 
     return (
         <div className="DictionarySearch">
             <form onSubmit={search}>
-                <input type="search" className="search-engine" placeholder="Search for a word..." />
+                <input type="search" onChange={handleChanges} className="search-engine" placeholder="Search for a word..." />
                 <input type="submit" className="search-submit-btn" value="🔎Go!" />
             </form>
             
