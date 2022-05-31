4f45e5de155b0feface7e8b8af40571d254882a8
Seo-Pt <seonah.pari.pt@gmail.com>
2022-05-31 14:27:30 +0200

    Prevent Default upon search

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch(){
+    
+    function search(event){
+        event.preventDefault();
+        alert("test");
+    }
+
     return (
         <div className="DictionarySearch">
-            <form>
+            <form onSubmit={search}>
                 <input type="search" className="search-engine" placeholder="Search for a word..." />
                 <input type="submit" className="search-submit-btn" value="🔎Go!" />
             </form>
