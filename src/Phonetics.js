ef325414a1b701d1101daa6152991021ab8a1c18
Seo-Pt <seonah.pari.pt@gmail.com>
2022-06-10 09:22:47 +0200

    Added new fav-icon, changed meta content

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,12 +1,12 @@
 import React from "react";
-import audioIcon from "./audio-icon.svg";
+import audioIcon from "./images/audio-icon.svg";
 
 export default function Phonetics(props){
     console.log(props)
     if (props.phoneticAudio){
         return (
         <div className="Phonetics">
-        <a href={props.phoneticAudio} className="me-4" target="_blank" rel="noreferrer" alt="Audio"><img src={audioIcon} className="audio-icon" width="30px" alt="Audio Icon"/></a>
+        <a href={props.phoneticAudio} className="me-4" target="_blank" rel="noreferrer" alt="Audio"><img src= {audioIcon} className="audio-icon" width="30px" alt="Audio Icon"/></a>
         <span>{props.phoneticText}</span>
         </div>
     )
