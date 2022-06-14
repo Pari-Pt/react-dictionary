3519bebad4dc92a972deaab96d7e29646c118812
Seo-Pt <seonah.pari.pt@gmail.com>
2022-06-14 12:59:59 +0200

    Updated links with attributes according to Netlify deploy log

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <h1>Dictionary App</h1>
       <DictionarySearch defaultKeyword="laugh" />
       <footer>
-      <div>This <a href="https://github.com/Seo-Pt/react-dictionary" target="_blank" rel="noopener">open-sourced</a> Dictionary was coded by <a href="https://confident-elion-25e30f.netlify.app/index.html" target="_blank" rel="noopener">Seonah Nathali</a></div>
+      <div>This <a href="https://github.com/Seo-Pt/react-dictionary" target="_blank" rel="noopener noreferrer">open-sourced</a> Dictionary was coded by <a href="https://confident-elion-25e30f.netlify.app/index.html" target="_blank" rel="noopener noreferrer">Seonah Nathali</a></div>
       </footer>
     </div>
   );
