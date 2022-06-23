8ff19a4a40e43ae6b84e49186068a8b3d9e3199e
Seo-Pt <seonah.pari.pt@gmail.com>
2022-06-23 14:27:39 +0200

    Adjusted styling of App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import DictionarySearch from "./DictionarySearch.js";
+import bookIcon from "./images/book-girl-icon.svg";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
+      <div className="image-heading"><img src={bookIcon} className="book-icon book-icon-left" width="250px"/>Dictionary App<img src={bookIcon} className="book-icon book-icon-right" width="250px"/></div>
       <DictionarySearch defaultKeyword="laugh" />
       <footer>
       <div>This <a href="https://github.com/Seo-Pt/react-dictionary" target="_blank" rel="noopener noreferrer">open-sourced</a> Dictionary was coded by <a href="https://confident-elion-25e30f.netlify.app/index.html" target="_blank" rel="noopener noreferrer">Seonah Nathali</a></div>
