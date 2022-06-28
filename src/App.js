bbe6ad5f779b9aeb18fe8a8526908f4184a51434
Seo-Pt <seonah.pari.pt@gmail.com>
2022-06-28 10:55:11 +0200

    Updated portfolio link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <div className="MainHeading"><img src={bookIcon} className="BookIcon BookIconLeft" alt="Girl with book" width="220px"/><span className="Title">Dictionary App</span><img src={bookIcon} className="BookIcon BookIconRight"  alt="Girl with book" width="220px"/></div>
       <DictionarySearch defaultKeyword="cheerful" />
       <footer>
-      <div>This <a href="https://github.com/Seo-Pt/react-dictionary" target="_blank" rel="noopener noreferrer">open-sourced</a> Dictionary was coded by <a href="https://confident-elion-25e30f.netlify.app/index.html" target="_blank" rel="noopener noreferrer">Seonah Nathali</a></div>
+      <div>This <a href="https://github.com/Pari-Pt/react-dictionary" target="_blank" rel="noopener noreferrer">open-sourced</a> Dictionary was coded by <a href="https://confident-elion-25e30f.netlify.app/index.html" target="_blank" rel="noopener noreferrer">S. Pari Nathali</a></div>
       </footer>
     </div>
   );
