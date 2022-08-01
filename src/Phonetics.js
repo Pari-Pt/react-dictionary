89f1b519d2b3d544a9acf7fc59f90f42c3023505
Seo-Pt <seonah.pari.pt@gmail.com>
2022-08-01 12:27:22 +0100

    Embedded Phonetics audio

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -6,6 +6,7 @@ export default function Phonetics(props){
     if (props.phonetics){
     if (props.phonetics[0].audio){
         return (
+
         <span className="Phonetics">
             <span><audio src={props.phonetics[0].audio} className="me-4" type="audio/mp3" controls><img src= {audioIcon} className="audio-icon" width="30px" alt="Audio Icon"/></audio></span>
             <div className="phonetics-text">{props.phonetics[0].text}</div>
@@ -14,6 +15,7 @@ export default function Phonetics(props){
         } else {
             if (props.phonetics[1].audio) {
                 return (
+
             <span className="Phonetics">
                 <span><audio src={props.phonetics[1].audio} className="me-4" type="audio/mp3" controls><img src= {audioIcon} className="audio-icon" width="30px" alt="Audio Icon"/></audio></span>
                 <div className="phonetics-text">{props.phonetics[1].text}</div>
@@ -22,6 +24,7 @@ export default function Phonetics(props){
             } else {
                     if (props.phonetics[2].audio) {
                         return (
+                            
                     <span className="Phonetics">
                         <span><audio src={props.phonetics[2].audio} className="me-4" type="audio/mp3" controls><img src= {audioIcon} className="audio-icon" width="30px" alt="Audio Icon"/></audio></span>
                         <div className="phonetics-text">{props.phonetics[2].text}</div>
